6df0fe3c574df997ed9c589703e2a19a4a6cf7ab
Goryudyuma <umaretekyoumade@gmail.com>
2016-07-09 08:14:42 +0900

    ウィンドウの表示

--- fsharpGUItest/fsharpGUItest/Program.fs
+++ fsharpGUItest/fsharpGUItest/Program.fs
@@ -1,7 +1,8 @@
 ﻿// F# の詳細については、http://fsharp.org を参照してください
 // 詳細については、'F# チュートリアル' プロジェクトを参照してください。
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // 整数の終了コードを返します
+open System;;
+open System.Windows.Forms;;
+
+[<STAThread>]
+do System.Windows.Forms.Application.Run(new Form());;
