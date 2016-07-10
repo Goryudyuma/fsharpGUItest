5bd3702f57a8a879954ac27464afbd333c8928fc
Goryudyuma <umaretekyoumade@gmail.com>
2016-07-10 21:12:40 +0900

    OKとNGを押すたびに変更するコード

--- fsharpGUItest/fsharpGUItest/Program.fs
+++ fsharpGUItest/fsharpGUItest/Program.fs
@@ -1,8 +1,16 @@
 ﻿// F# の詳細については、http://fsharp.org を参照してください
 // 詳細については、'F# チュートリアル' プロジェクトを参照してください。
 
-open System;;
-open System.Windows.Forms;;
+open System 
+open System.Windows.Forms 
+open System.Drawing 
+ 
+//do 
+//    Application.Run(mainForm) // #2 
+let form = new Form(Text = "ボタンテスト")
+let btn = new Button(Text = "OK")
+btn.Click.AddHandler(fun _ _ -> (if btn.Text = "OK" then btn.Text <- "NG" else btn.Text <- "OK") |> ignore)
 
-[<STAThread>]
-do System.Windows.Forms.Application.Run(new Form());;
+form.Controls.Add(btn)
+
+Application.Run(form)
