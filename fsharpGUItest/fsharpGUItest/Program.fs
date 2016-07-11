a41280a02ddea895b7ed5caf6f1ed6ea7fe2245f
Goryudyuma <umaretekyoumade@gmail.com>
2016-07-11 20:33:02 +0900

    ボタンをたくさん生み出すコード arrayを使ってみたかった

--- fsharpGUItest/fsharpGUItest/Program.fs
+++ fsharpGUItest/fsharpGUItest/Program.fs
@@ -7,10 +7,23 @@ open System.Drawing
  
 //do 
 //    Application.Run(mainForm) // #2 
-let form = new Form(Text = "ボタンテスト")
+let form = new Form(Text = "ボタンテスト", Height = 600,Width = 800)
 let btn = new Button(Text = "OK")
-btn.Click.AddHandler(fun _ _ -> (if btn.Text = "OK" then btn.Text <- "NG" else btn.Text <- "OK") |> ignore)
 
-form.Controls.Add(btn)
+
+let array = [|for i in 0..10 -> [|for j in 0..10 -> new Button(Text = "OK", Top = i * 30, Left = j * 70) |] |]
+
+for i in 0..10 do
+    for j in 0..10 do
+        array.[i].[j].Click.AddHandler(fun _ _ -> (if array.[i].[j].Text = "OK" then array.[i].[j].Text <- "NG" else array.[i].[j].Text <- "OK") |> ignore)
+        form.Controls.Add(array.[i].[j])
+    done
+done
+        
+
+
+//btn.Click.AddHandler(fun _ _ -> (if btn.Text = "OK" then btn.Text <- "NG" else btn.Text <- "OK") |> ignore)
+
+//form.Controls.Add(btn)
 
 Application.Run(form)
