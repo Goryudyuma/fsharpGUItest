71bd19209864cf87640376951a605c31c2ef050e
Goryudyuma <umaretekyoumade@gmail.com>
2016-07-12 21:11:49 +0900

    Button classの継承

--- fsharpGUItest/fsharpGUItest/Program.fs
+++ fsharpGUItest/fsharpGUItest/Program.fs
@@ -10,8 +10,20 @@ open System.Drawing
 let form = new Form(Text = "ボタンテスト", Height = 600,Width = 800)
 let btn = new Button(Text = "OK")
 
+type Square = class
+    inherit Button
+    val mutable isBomb : Boolean
+    val mutable isOpened : Boolean
+    val mutable count : int
+    new(text, top, left) = {inherit Button(Text = text, Top = top,  Left = left);isBomb = false; isOpened = false; count = 0}
 
-let array = [|for i in 0..10 -> [|for j in 0..10 -> new Button(Text = "OK", Top = i * 30, Left = j * 70) |] |]
+    member x.changeIsBomb input = x.isBomb <- input
+
+end
+
+
+
+let array = [|for i in 0..10 -> [|for j in 0..10 -> new Square("OK" , i * 30 , j * 70) |] |]
 
 for i in 0..10 do
     for j in 0..10 do
